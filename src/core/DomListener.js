651c83ab544bdce3d33d82c78e743ad854e64dd2
Dmitry Shibikin <sdd@lab.local>
2020-12-02 10:11:14 +0700

    ls35 - add listener

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,32 @@
-export class DomListener {
+import {capitalize} from './utils'
 
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provided for DomListener!`)
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
+  initDomListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      console.log(method)
+      if (!this[method]) {
+        throw new Error(
+            `Method ${method} is not implement in ${this.name || ''} Component`
+        )
+      }
+      this.$root.on(listener, this[method].bind(this))
+    })
+  }
+
+  removeDomListeners() {
+
+  }
+}
+
+// input => OnInput
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
