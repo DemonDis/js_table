73e543b9e0a6236137aad4136b478f1bd3de1345
Dmitry Shibikin <sdd@lab.local>
2021-01-09 11:25:09 +0700

    resize element

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -5,22 +5,10 @@ export class Table extends ExcelComponent {
   static className = 'excel__table'
   constructor($root) {
     super($root, {
-      listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
     })
   }
   toHTML() {
     return createTable()
-  }
-  onClick() {
-    console.log('click')
-  }
-  onMousedown(event) {
-    console.log('mousedown', event.target)
-  }
-  onMousemove() {
-    console.log('mousemove')
-  }
-  onMouseup() {
-    console.log('mouseup')
   }
 }
